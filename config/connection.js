59803bc7939fb32af158bb1977c94f6ce9c04fe7
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-05 16:01:18 -0500

    Established DB connection

--- config/connection.js
+++ config/connection.js
@@ -9,7 +9,6 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
