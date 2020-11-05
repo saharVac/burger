b62a41d513e96e84672d0fe9a9448f345d447b6e
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-05 16:32:44 -0500

    Set the routing requests

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,46 @@
+// DEPENDENCIES
+const express = require("express")
+const Burger = require("../models/burger.js")
+
+// ROUTER
+var router = express.Router();
+
+router.get("/", function(req, res) {
+    Burger.all(function(data) {
+      var hbsObject = {
+        cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+    });
+  });
+  
+  router.post("/api/burgers", function(req, res) {
+    Burger.create(["name", "sleepy"], [req.body.name, req.body.sleepy], function(result) {
+      // Send back the ID of the new quote
+      res.json({ id: result.insertId });
+    });
+  });
+  
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+    console.log("condition", condition);
+  
+    Burger.update(
+      {
+        devoured: req.body.devoured
+      },
+      condition,
+      function(result) {
+        if (result.changedRows === 0) {
+          // If no rows were changed, then the ID must not exist, so 404
+          return res.status(404).end();
+        }
+        res.status(200).end();
+  
+      }
+    );
+  });
+
+module.exports = router;
