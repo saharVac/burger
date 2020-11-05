47f721656fe80cb709fb9bee5a4d0cb99a84e4e0
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-05 16:11:00 -0500

    Added SQL to set the database, the table, and populate it

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Mushroom Swiss Burger');
+INSERT INTO burgers (burger_name) VALUES ('Elk Burger');
+INSERT INTO burgers (burger_name) VALUES ("Sarah's Burger");
+INSERT INTO burgers (burger_name) VALUES ('Marilyn Manson Burger');
+INSERT INTO burgers (burger_name) VALUES ('Joe Biden Burger');
+INSERT INTO burgers (burger_name) VALUES ('Owens Burger');
