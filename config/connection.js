5f05ebbccc89f7fac3d46ec76717b571848e98e3
Sahar Vacnich <saharvacnich@Sahars-MacBook-Pro.local>
2020-11-06 10:10:21 -0500

    Added JAWSDB connection possibility

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+}
 
+// connect
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
